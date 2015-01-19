5c836f83ffd8a67eafabee0a72a612688451cbb1
Kristopher Wilson <kristopherwilson@gmail.com>
2015-01-18 23:22:47 -0500

    updating peridot to watch module/

--- peridot.php
+++ peridot.php
@@ -7,6 +7,7 @@ use Peridot\Plugin\Watcher\WatcherPlugin;
 return function(EventEmitterInterface $emitter) {
     $watcher = new WatcherPlugin($emitter);
     $watcher->track(__DIR__ . '/src');
+    $watcher->track(__DIR__ . '/module');
 
     new ProphecyPlugin($emitter);
 };
