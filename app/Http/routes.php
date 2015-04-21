6503ce30aa10eaedcdd77409ee4e217939cf2db6
Kristopher Wilson <kristopherwilson@gmail.com>
2015-04-21 13:15:24 -0400

    Adding Order related routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,3 +5,7 @@ Route::get('/', 'DashboardController@indexAction');
 Route::get('/customers', 'CustomersController@indexAction');
 Route::match(['get', 'post'], '/customers/new', 'CustomersController@newOrEditAction');
 Route::match(['get', 'post'], '/customers/edit/{id}', 'CustomersController@newOrEditAction');
+
+Route::get('/orders', 'OrdersController@indexAction');
+Route::match(['get', 'post'], '/orders/new', 'OrdersController@newAction');
+Route::get('/orders/view/{id}', 'OrdersController@viewAction');
